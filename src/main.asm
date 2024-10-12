c3e454d1e8dbfcb83ca625486c6a68ee21ed8cd7
iminsgineark <ankurjha4025@gmail.com>
2024-10-12 13:43:23 +0000

    updated main.asm file

--- src/main.asm
+++ src/main.asm
@@ -3,48 +3,47 @@ bits 16
 
 %define ENDL 0x0D, 0x0A
 
-start: 
-	jmp	main
+start:
+    jmp main
 
 puts:
-	push si
-	push ax
+    push si
+    push ax
 
 .loop:
-	lodsb
-	or al, al
-	jz .done
-	jmp .loop
+    lodsb               
+    or al, al           
+    jz .done            
+    
+    mov ah, 0x0e        
+    int 0x10            
 
-	mov ah, 0x0e
-	mov bh, 0
-	int 0x10
-
-	jmp .loop
-	
+    jmp .loop           
+    
 .done:
-	pop ax
-	pop si
-	ret	
+    pop ax
+    pop si
+    ret
 
 main:
+    
+    xor ax, ax          
+    mov ds, ax          
+    mov es, ax          
 
-	mov ax, 0
-	mov ds, ax
-	mov es, ax
 
-	mov ss, ax
-	mov sp, 0x7C00
+    mov ss, ax
+    mov sp, 0x7C00      
 
-	mov si, msg_hello
-	call puts
+    mov si, msg_hello
+    call puts
 
-	hlt
+    hlt                 
 
 .halt:
-	jmp .halt
+    jmp .halt           
+    
+msg_hello: db 'Welcome to Ark OS', ENDL, 0
 
-msg_hello: db 'Welcome to Ark OS', ENDL ,0
-
-times 510-($-$$) db 0
-dw 0AA55h
+times 510-($-$$) db 0   
+dw 0xAA55               
